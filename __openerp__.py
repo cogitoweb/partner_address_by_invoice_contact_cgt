19a2a946df2004849cdfba9a56ebe2b4bf5aa284
2z <paolo.cazzitti@cogitoweb.it>
2017-02-22 10:50:22 +0100

    init

---
+++ __openerp__.py
@@ -0,0 +1,24 @@
+# -*- coding: utf-8 -*-
+{
+	'name': "Partner Address by Invoice Contact",
+	'summary': "",
+	'author': "Cogito",
+	'website': "http://www.cogitoweb.it",
+	'description': "Display customer/supplier partner address depending on contact of type invoice",
+
+	# Categories can be used to filter modules in modules listing
+	# Check https://github.com/odoo/odoo/blob/master/openerp/addons/base/module/module_data.xml
+	# for the full list
+	'category': 'Extra Tools',
+	'version': '0.1',
+
+	# any module necessary for this one to work correctly
+	'depends': ['base'],
+
+	# always loaded
+	'data': [
+
+	],
+
+	'installable': True
+}
---
+++ model/res_partner.py
@@ -0,0 +1,21 @@
+from odoo import api, fields, models, tools, SUPERUSER_ID, _
+
+import logging
+_logger = logging.getLogger(__name__)
+
+class ResPartner(models.Model):
+
+    _inherit = 'res.partner'
+
+    @api.multi
+    def _display_address(self, without_company=False):
+    
+        me = self
+        if(self.customer or self.supplier): # only for customers or suppliers
+            if(self.child_id): # which has childs (contatcs)
+                # of type invoice, active, not company, without use of parent address
+                childs = self.child_ids.filtered(lambda r: r.type == "invoice" r.is_company == False and r.use_parent_address == False and r.active=True)
+                if(childs): 
+                    me = childs[0]
+        
+        return super(me, without_company)
